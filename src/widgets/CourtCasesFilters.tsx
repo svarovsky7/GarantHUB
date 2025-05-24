7b4476ea63670999e8b0e9d97ec1e8a2fa450a20
svarovsky7 <46369931+svarovsky7@users.noreply.github.com>
2025-05-25 00:23:52 +0300

    fix: update Grid import and resolver generics

--- src/widgets/CourtCasesFilters.tsx
+++ src/widgets/CourtCasesFilters.tsx
@@ -7,7 +7,7 @@ import {
     Select,
     Button,
 } from '@mui/material';
-import Grid from '@mui/material/Grid';
+import Grid from '@mui/material/Unstable_Grid2';
 
 const statusesRu = [
     { value: '', label: 'Все статусы' },
