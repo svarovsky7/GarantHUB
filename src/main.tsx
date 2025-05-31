a9b300b77f70b71b38224983d8ce5e5b051cc967
svarovsky7 <46369931+svarovsky7@users.noreply.github.com>
2025-05-31 07:30:55 +0300

    fix: add MUI date pickers provider

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,22 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "@/shared/config/queryClient";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs from "dayjs";
+import "dayjs/locale/ru";
+
+dayjs.locale("ru");
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ru">
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </LocalizationProvider>
   </React.StrictMode>,
 );
