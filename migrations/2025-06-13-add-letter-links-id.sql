33211f2acc32fb83e142edbe7bca4097606e6a92
svarovsky7 <46369931+svarovsky7@users.noreply.github.com>
2025-06-12 18:54:21 +0300

    fix migration for letter links id

--- migrations/2025-06-13-add-letter-links-id.sql
+++ migrations/2025-06-13-add-letter-links-id.sql
@@ -1,3 +1,6 @@
 -- Добавляем уникальный идентификатор в таблицу связей писем
 ALTER TABLE letter_links
-  ADD COLUMN id BIGSERIAL PRIMARY KEY;
+  ADD COLUMN id BIGSERIAL;
+
+ALTER TABLE letter_links
+  ADD CONSTRAINT letter_links_id_key UNIQUE (id);
