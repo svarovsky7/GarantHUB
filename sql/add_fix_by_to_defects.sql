5cf81efeb07d7fba0b74cb436a6167477cc596df
svarovsky7 <svarovsky7@gmail.com>
2025-06-15 08:25:05 +0300

    Корректирую схему 24

--- sql/add_fix_by_to_defects.sql
+++
@@ -1,2 +0,0 @@
--- Track performer responsible for fixing defect
-ALTER TABLE defects ADD COLUMN IF NOT EXISTS fix_by text;
--- sql/add_fixed_at_to_defects.sql
+++
@@ -1,2 +0,0 @@
--- Add fixed_at column to defects table
-ALTER TABLE defects ADD COLUMN IF NOT EXISTS fixed_at date;
