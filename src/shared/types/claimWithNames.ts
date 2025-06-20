f71680d1a07e58bcb7874f0b15c26642d8c3ef20
svarovsky7 <46369931+svarovsky7@users.noreply.github.com>
2025-06-20 06:56:30 +0300

    fix claims query

--- src/shared/types/claimWithNames.ts
+++ src/shared/types/claimWithNames.ts
@@ -1,5 +1,23 @@
 import type { Claim } from '@/shared/types/claim';
+import type { Dayjs } from 'dayjs';
 
 export interface ClaimWithNames extends Claim {
+  /** Название проекта, к которому относится претензия */
+  projectName: string;
+  /** Наименование статуса претензии */
+  statusName: string;
+  /** Цвет статуса для отображения */
+  statusColor: string | null;
+  /** ФИО ответственного инженера */
   responsibleEngineerName: string | null;
+  /** Список объектов одной строкой */
+  unitNames?: string;
+  /** Дата претензии */
+  claimDate: Dayjs | null;
+  /** Дата получения претензии застройщиком */
+  receivedByDeveloperAt: Dayjs | null;
+  /** Дата регистрации претензии */
+  registeredAt: Dayjs | null;
+  /** Дата устранения претензии */
+  fixedAt: Dayjs | null;
 }
