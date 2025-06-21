f892da90fcb0b78ddaab7b4ae3bee1de557efb67
svarovsky7 <46369931+svarovsky7@users.noreply.github.com>
2025-06-21 21:55:19 +0300

    Revert "Move claim edit actions"

--- src/shared/types/claimFormAntdEditRef.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Интерфейс управления формой редактирования претензии.
- */
-export interface ClaimFormAntdEditRef {
-  /** Отправить форму */
-  submit: () => void;
-  /** Идет ли отправка формы */
-  isSubmitting: boolean;
-}
