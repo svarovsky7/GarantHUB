a6be32b3c82a0e3ed39da1d127a90428dc410d02
svarovsky7 <svarovsky7@gmail.com>
2025-06-22 17:03:36 +0300

    Корректирую схему 28

--- migrations/remove_tickets.sql
+++
@@ -1,6 +0,0 @@
--- Удаление таблиц, связанных с замечаниями
-DROP TABLE IF EXISTS claim_tickets;
-DROP TABLE IF EXISTS ticket_attachments;
-DROP TABLE IF EXISTS ticket_links;
-DROP TABLE IF EXISTS ticket_units;
-DROP TABLE IF EXISTS tickets;
