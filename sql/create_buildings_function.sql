dc72e18bdd1a237b954360c5b0e037488168a146
svarovsky7 <svarovsky7@gmail.com>
2025-06-23 22:18:15 +0300

    Корректирую схему 28

--- sql/create_buildings_function.sql
+++
@@ -1,9 +0,0 @@
--- Функция для получения списка корпусов проекта
-CREATE OR REPLACE FUNCTION buildings_by_project(pid integer)
-RETURNS TABLE(building text)
-LANGUAGE sql STABLE AS $$
-  SELECT DISTINCT building
-  FROM units
-  WHERE project_id = pid AND building IS NOT NULL
-  ORDER BY building;
-$$;
