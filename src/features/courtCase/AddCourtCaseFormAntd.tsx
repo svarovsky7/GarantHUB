45268c54c7c94583bf20c104701f03ab31e6e598
svarovsky7 <46369931+svarovsky7@users.noreply.github.com>
2025-06-25 07:45:34 +0300

    Revert "Update case UID logic"

--- src/features/courtCase/AddCourtCaseFormAntd.tsx
+++ src/features/courtCase/AddCourtCaseFormAntd.tsx
@@ -156,7 +156,6 @@ export default function AddCourtCaseFormAntd({
   const handleAddCase = async (values: any) => {
     try {
       const uidId = await getOrCreateCaseUid(values.case_uid);
-      qc.invalidateQueries({ queryKey: ['case_uids'] });
       const newCase = await addCaseMutation.mutateAsync({
         project_id: values.project_id,
         unit_ids: values.unit_ids || [],
