c990223a6f82f551c379af20d784927af2f1a671
svarovsky7 <svarovsky7@gmail.com>
2025-06-26 08:01:45 +0300

    Корректирую схему 28

--- sql/claims_owner.sql
+++
@@ -1,3 +0,0 @@
--- Удаляем связь с физлицом и добавляем поле владельца объекта
-ALTER TABLE claims DROP COLUMN IF EXISTS person_id;
-ALTER TABLE claims ADD COLUMN IF NOT EXISTS owner text;
--- sql/pretrial_columns.sql
+++
@@ -1,3 +0,0 @@
--- Добавление признака досудебной претензии
-ALTER TABLE claims ADD COLUMN pre_trial_claim boolean NOT NULL DEFAULT false;
-ALTER TABLE claim_defects ADD COLUMN pre_trial_claim boolean NOT NULL DEFAULT false;
