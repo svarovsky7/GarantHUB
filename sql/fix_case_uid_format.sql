68475e2d9e8f85e8c6d7a75bf053a7ce00caadc5
svarovsky7 <svarovsky7@gmail.com>
2025-07-05 19:42:10 +0300

    Корректирую схему 30

--- sql/fix_case_uid_format.sql
+++
@@ -1,5 +0,0 @@
--- Удалить кавычки и квадратные скобки из поля uid
-UPDATE court_cases_uids
-SET uid = regexp_replace(uid, '^\["(.+)"\]$', '\1')
-WHERE uid ~ '^\[".*"\]$';
-
--- sql/rename_case_uids_to_court_cases_uids.sql
+++
@@ -1,7 +0,0 @@
--- Переименование таблицы case_uids в court_cases_uids
-ALTER TABLE case_uids RENAME TO court_cases_uids;
--- Переименование последовательности для id
-ALTER SEQUENCE case_uids_id_seq RENAME TO court_cases_uids_id_seq;
--- Переименование ограничений первичного ключа и уникальности
-ALTER INDEX case_uids_pkey RENAME TO court_cases_uids_pkey;
-ALTER INDEX case_uids_uid_key RENAME TO court_cases_uids_uid_key;
