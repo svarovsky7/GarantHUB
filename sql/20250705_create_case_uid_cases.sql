567917e2d3dc563bff3a4b6dbd06f3405a72d590
svarovsky7 <46369931+svarovsky7@users.noreply.github.com>
2025-07-05 16:59:59 +0300

    Revert "feat: add table for linking cases to uid"

--- sql/20250705_create_case_uid_cases.sql
+++
@@ -1,6 +0,0 @@
--- SQL миграция: связывает уникальные идентификаторы с судебными делами
-CREATE TABLE IF NOT EXISTS case_uid_cases (
-  case_uid_id integer NOT NULL REFERENCES case_uids(id) ON DELETE CASCADE,
-  court_case_id bigint NOT NULL REFERENCES court_cases(id) ON DELETE CASCADE,
-  PRIMARY KEY (case_uid_id, court_case_id)
-);
